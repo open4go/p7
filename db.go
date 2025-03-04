c3321a976774542ce0f0cea6378eb9c4570051e6
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2025-03-05 01:21:58 +0800

    add cache

---
+++ db.go
@@ -0,0 +1,17 @@
+package user
+
+import (
+	"context"
+	xredis "github.com/open4go/db/redis"
+	"github.com/open4go/log"
+	"github.com/redis/go-redis/v9"
+)
+
+// GetRedisCacheHandler 获取数据库handler 这里定义一个方法
+func GetRedisCacheHandler(ctx context.Context) *redis.Client {
+	handler, err := xredis.DBPool.GetHandler("cache")
+	if err != nil {
+		log.Log(ctx).Fatal(err)
+	}
+	return handler
+}
